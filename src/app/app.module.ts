13d74fa2d628498aed202fca0cd13c0d23f45944
Verzach3 <gabrielcorreacardenas@gmail.com>
2021-03-04 19:40:48 -0500

    First Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//imports mios
+  //Importo el environment (que tiene todas las claves de la api)
+import { environment } from "src/environments/environment";
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestore, AngularFirestoreModule } from "@angular/fire/firestore";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+
+    //Imports mios
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from '../services/firebase.service';
+
+interface ProductData {
+  Name: string;
+  Age: number;
+  Addres: string;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  productList = [];
+  productData: ProductData;
+  productForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private firebaseService: FirebaseService,
+    public fb: FormBuilder
+  ) {
+    this.productData = {} as ProductData;
+  }
 
+  ngOnInit() {
+    this.productForm = this.fb.group({
+      Name: ['', [Validators.required]],
+      Age: ['', [Validators.required]],
+      Address: ['', [Validators.required]],
+    });
+
+    this.firebaseService.read_products().subscribe((data) => {
+      this.productList = data.map((e) => {
+        return {
+          id: e.payload.doc['id'],
+          isEdit: false,
+          Name: e.payload.doc.data()['Name'],
+          Age: e.payload.doc.data()['Age'],
+          Address: e.payload.doc.data()['Address'],
+        };
+      });
+    });
+  }
+
+  CreateRecord() {
+    this.firebaseService.create_product(this.productForm.value).then((resp) => {
+      //Reset form
+      this.productForm.reset();
+    });
+  }
+
+  RemoveRecord(rowID) {
+    this.firebaseService.delete_product(rowID);
+  }
+
+  EditRecord(record) {
+    record.isEdit = true;
+    record.EditName = record.Name;
+    record.EditAge = record.Age;
+    record.EditAddress = record.Address;
+  }
+
+  UpdateRecord(recordRow) {
+    let record = {};
+    record['Name'] = recordRow.EditName;
+    record['Age'] = recordRow.EditAge;
+    record['Address'] = recordRow.EditAddress;
+    this.firebaseService.update_product(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
 }
